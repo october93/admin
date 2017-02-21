dfd76229f4dc5104c86ee3772a13417de0fe2ea5
Paul Bohm <paul@october.news>
2017-02-20 22:35:30 -0800

    generate menu from state

--- src/components/MyMenu/index.js
+++ src/components/MyMenu/index.js
@@ -7,17 +7,31 @@ import classnames from 'classnames';
 import {Menu, MenuItem, Alignments} from 'react-foundation';
 
 export default class MyMenu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      menuItems: [
+        {name: "Dashboard", path: "/"},
+        {name: "Users", path: "/users/"},
+        {name: "Cards", path: "/cards/"},
+        {name: "Settings", path: "/settings/"},
+        {name: "Experiments", path: "/experiments/"},
+        {name: "Graph", path: "/graph/"}
+      ]
+    };
+  }
+  renderMenuItems() {
+    return this.state.menuItems.map(item => (
+        <MenuItem><a href={item.path}>{item.name}</a></MenuItem>
+      )
+    )
+  }
   render() {
     const { className, ...props } = this.props;
     return (
       <div className={classnames("MyMenu", className)} {...props}>
         <Menu alignment={Alignments.CENTER} isExpanded>
-          <MenuItem><a href="/">Dashboard</a></MenuItem>
-          <MenuItem><a href="/users/">Users</a></MenuItem>
-          <MenuItem><a href="/cards/">Cards</a></MenuItem>
-          <MenuItem><a href="/settings/">Settings</a></MenuItem>
-          <MenuItem><a href="/experiments/">Experiments</a></MenuItem>
-          <MenuItem><a href="/graph/">Graph</a></MenuItem>
+          {this.renderMenuItems()}
         </Menu>
       </div>
     );
