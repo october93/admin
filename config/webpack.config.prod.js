bb77b1cd79225b80d723151ee3554c02503aa730
Konrad Reiche <konrad.reiche@gmail.com>
2017-03-09 01:17:56 -0800

    Apply Sass loader to webpack production, too

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -110,7 +110,8 @@ module.exports = {
           /\.(js|jsx)$/,
           /\.css$/,
           /\.json$/,
-          /\.svg$/
+          /\.svg$/,
+          /\.scss$/
         ],
         loader: 'url',
         query: {
@@ -159,6 +160,11 @@ module.exports = {
         query: {
           name: 'static/media/[name].[hash:8].[ext]'
         }
+      },
+      {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loader: 'style!css!sass?sourceMap'
       }
       // ** STOP ** Are you adding a new loader?
       // Remember to add the new extension(s) to the "url" loader exclusion list.
