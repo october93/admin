0ee416cee35653f8f55c54c73ded88b8a7a10307
Konrad Reiche <konrad.reiche@gmail.com>
2017-03-09 17:32:57 -0800

    Use wss in case admin panel is hosted on https

--- src/store/AdminStore.js
+++ src/store/AdminStore.js
@@ -5,7 +5,7 @@ import { observable } from 'mobx';
 import uuid from "uuid"
 
 
-const defaultServerURL = `ws://${location.hostname}:8080`
+const defaultServerURL = `${location.hostname}:8080`
 
 const MSG_SUCCESS = "success"
 const MSG_FAIL = "failure"
@@ -40,7 +40,11 @@ class AdminStore {
 
 	constructor() {
 		// setup SockJS
-    this.serverURL = defaultServerURL
+    let wsProtocol = 'ws:'
+    if (location.protocol == 'https:') {
+      wsProtocol = 'wss:'
+    }
+    this.serverURL = `${wsProtocol}//${defaultServerURL}`
 
 		const sockURL = `${this.serverURL}/deck_endpoint/`
 		this.socket = new WebSocket(sockURL)
