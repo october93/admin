3d57d508efad76ee0121c410e273c90c52d4de7e
Konrad Reiche <konrad.reiche@gmail.com>
2017-03-09 17:25:15 -0800

    Delete admin directory before copying new files

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -19,5 +19,8 @@ npm run build
 
 curl -X POST -H 'Content-type: application/json' --data "{'text': 'Deploying admin panel to $HOST'}" https://hooks.slack.com/services/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
 
+# Delete old files
+ssh $REMOTE_USER@$HOST "rm -rf admin"
+
 # Copy files to host
 scp -r build $REMOTE_USER@$HOST:./admin
