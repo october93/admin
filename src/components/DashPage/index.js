7a7a0917d8806c3476b1a4170a17004c3dded0ea
Konrad Reiche <konrad.reiche@gmail.com>
2017-05-26 14:47:42 +0200

    Fetch user profile info and store it

--- src/components/DashPage/index.js
+++ src/components/DashPage/index.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { Button } from "react-foundation"
 
 import './style.scss';
 
 @inject("store") @observer
 class DashPage extends Component {
-  constructor(props){
-    super(props)
-
-    this.connectToProd = this.connectToProd.bind(this)
-    this.connectToLocal = this.connectToLocal.bind(this)
-  }
-
-  connectToProd(){
-    this.props.store.changeEndpoint("wss://testing.october.news")
-  }
-
-  connectToLocal(){
-    this.props.store.changeEndpoint("ws://localhost:8080")
-  }
-
   render() {
+    const profile = JSON.parse(localStorage.getItem("profile"))
     return (
         <div>
-        This is the October Dashboard. We display important things here.
-        <div>
-          <Button onClick={this.connectToProd}>Connect To testing.october.news</Button>
-          <div>
-            <Button onClick={this.connectToLocal}>Connect To local</Button>
-
-          </div>
-        </div>
+          Welcome {profile.given_name}
         </div>
 
     );
--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -1,13 +1,26 @@
 import Auth0Lock from 'auth0-lock'
 
+const options = {
+  allowedConnections: ['google-oauth2'],
+  allowSignUp: false
+}
+
 export default class AuthService {
   constructor(clientID, domain) {
-    this.lock = new Auth0Lock(clientID, domain, {allowedConnections: ['google-oauth2']})
+    this.lock = new Auth0Lock(clientID, domain, options)
     this.lock.on('authenticated', this._doAuthentication.bind(this))
     this.login = this.login.bind(this)
   }
 
   _doAuthentication(authResult) {
+    this.lock.getUserInfo(authResult.accessToken, function(error, profile) {
+      if (error) {
+        console.log(error)
+        return;
+      }
+      localStorage.setItem("accessToken", authResult.accessToken);
+      localStorage.setItem("profile", JSON.stringify(profile));
+    });
     this.setToken(authResult.idToken)
   }
 
