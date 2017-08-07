0231ccb3d5e73bc7284788274cf09b633c401136
Konrad Reiche <konrad.reiche@gmail.com>
2017-08-07 10:09:29 -0700

    Fix linting errors

--- src/components/RPCHistoryItem/index.js
+++ src/components/RPCHistoryItem/index.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react';
-
 import './style.scss'
 
 export default class RPCHistoryItem extends Component {
-
-  constructor(props){
-    super(props)
-  }
-
   render() {
     return (
       <li className="historyItem" onClick={() => this.props.handleClick(this.props.content)}><code>{this.props.content}</code></li>
