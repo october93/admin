12702d99cbb747b4a8a7adbb26a1b7c54e6ee659
Konrad Reiche <konrad.reiche@gmail.com>
2017-08-07 10:50:33 -0700

    Fix styling of RPC history for long inputs

--- src/components/RPCHistoryItem/index.js
+++ src/components/RPCHistoryItem/index.js
@@ -4,7 +4,11 @@ import './style.scss'
 export default class RPCHistoryItem extends Component {
   render() {
     return (
-      <li className="historyItem" onClick={() => this.props.handleClick(this.props.content)}><code>{this.props.content}</code></li>
+      <li className="historyItem" onClick={() => this.props.handleClick(this.props.content)}>
+        <code>
+          <pre>{this.props.content}</pre>
+        </code>
+      </li>
     )
   }
 }
