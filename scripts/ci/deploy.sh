55606b0e21706ff2bd83aa9b4c7d4f27b5dc2e89
Christopher Gilson <c.gilson11@gmail.com>
2018-02-20 11:22:50 -0800

    deploying to engine too

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -2,4 +2,5 @@
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ENV=latest ./scripts/ci/deploy_to_env.sh
   ENV=development ./scripts/ci/deploy_to_env.sh
+  ENV=engine ./scripts/ci/deploy_to_env.sh
 fi
