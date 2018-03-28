ae6dc5d5031b586d859a4ee40bf1f3424b87c939
Konrad Reiche <konrad.reiche@gmail.com>
2018-03-28 16:01:52 -0700

    Update host endpoints for deployment

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ENV=latest ./scripts/ci/deploy_to_env.sh
-  ENV=development ./scripts/ci/deploy_to_env.sh
-  ENV=engine ./scripts/ci/deploy_to_env.sh
+  ENV=engine.internal.development ./scripts/ci/deploy_to_env.sh
+  ENV=engine.internal ./scripts/ci/deploy_to_env.sh
 fi
