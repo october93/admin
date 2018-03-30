daf20f40d358d774b47028f05afeafb101c20f73
Konrad Reiche <konrad.reiche@gmail.com>
2018-03-30 09:33:25 -0700

    Split deployment between master and development

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ENV=engine.internal.development ./scripts/ci/deploy_to_env.sh
   ENV=engine.internal ./scripts/ci/deploy_to_env.sh
+fi
+
+if [ "$TRAVIS_BRANCH" == "development" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ENV=engine.internal.development ./scripts/ci/deploy_to_env.sh
 fi
