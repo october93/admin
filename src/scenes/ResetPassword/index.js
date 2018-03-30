d74501552e41b60475d4a89376bd107b435e78bb
Konrad Reiche <konrad.reiche@gmail.com>
2018-03-29 20:11:38 -0700

    Style login page

--- src/scenes/ResetPassword/index.js
+++ src/scenes/ResetPassword/index.js
@@ -22,6 +22,8 @@ const Submit = glamorous.input({
   color: "#fff",
   borderRadius: "5px",
   display: "block",
+  fontSize: "15px",
+  fontWeight: "bold",
   margin: "0 auto",
   backgroundColor: "#1aafdb",
   textAlign: "center",
@@ -32,6 +34,21 @@ const Submit = glamorous.input({
   "&:active": {
     backgroundColor: "#1a88db",
   },
+})
+
+const Input = glamorous.input({
+  margin: "0 auto 1rem auto",
+  padding: "5px",
+  height: "30px",
+  width: "15rem",
+  display: "block",
+  fontFamily: "inherit",
+  borderRadius: "3px",
+  borderWidth: "1px",
+  borderStyle: "solid",
+  borderColor: "rgb(206, 208, 212)",
+  borderImage: "initial",
+  outline: "none",
 })
 
 class ResetPasswordPage extends Component {
@@ -53,7 +70,7 @@ class ResetPasswordPage extends Component {
       <ResetPasswordContainer>
         <ResetPasswordForm>
           <form action="/" onSubmit={this.handleSubmit}>
-            <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleChange} />
+            <Input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
             <Submit type="submit" value="Reset Password" />
           </form>
         </ResetPasswordForm>
