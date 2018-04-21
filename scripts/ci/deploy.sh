45c1bc016449ed1f7ed18b4ffc1e105ac7fd58b9
Christopher Gilson <c.gilson11@gmail.com>
2018-04-21 15:44:59 -0700

    wrong graphql origin

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -3,7 +3,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
   export REACT_APP_ENVIRONMENT=prod
   export REACT_APP_APP_HOST=https://october.news
   export REACT_APP_API_ENDPOINT=wss://engine.october.news/deck_endpoint/
-  export REACT_APP_GRAPHQL_ENDPOINT=https://october.news/graphql
+  export REACT_APP_GRAPHQL_ENDPOINT=https://engine.october.news/graphql
   export REACT_APP_ALGOLIA_ENVIRONMENT=engine
   ENV=engine.internal ./scripts/ci/deploy_to_env.sh
 fi
@@ -12,7 +12,7 @@ if [ "$TRAVIS_BRANCH" == "development" ] && [ "$TRAVIS_PULL_REQUEST" == "false" 
   export REACT_APP_ENVIRONMENT=dev
   export REACT_APP_APP_HOST=https://development.october.news
   export REACT_APP_API_ENDPOINT=wss://engine.development.october.news/deck_endpoint/
-  export REACT_APP_GRAPHQL_ENDPOINT=https://development.october.news/graphql
+  export REACT_APP_GRAPHQL_ENDPOINT=https://engine.development.october.news/graphql
   export REACT_APP_ALGOLIA_ENVIRONMENT=development_engine
 
   ENV=engine.internal.development ./scripts/ci/deploy_to_env.sh
