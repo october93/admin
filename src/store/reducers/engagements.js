f3cee82aade0f177ba33e5ff76a23f6d9e27e9fe
Konrad Reiche <konrad.reiche@gmail.com>
2018-10-09 15:36:39 -0700

    Fix engagement panel & consolidate

--- src/store/reducers/engagements.js
+++ src/store/reducers/engagements.js
@@ -3,9 +3,23 @@ import {
 } from '../actions/creators/types'
 
 export const engagements = (state = [], action) => {
+	const excludeUsernames = {
+		paul: true,
+		eugene: true,
+		chris: true,
+		konrad: true,
+		kai: true,
+		kingsley: true,
+		juan: true,
+		tomas: true,
+		richagoyal: true,
+		root: true,
+		october: true,
+	}
+
 	switch (action.type) {
 		case GET_ENGAGEMENT_SUCCESS:
-			return action.engagements
+			return action.engagements.filter(engagement => !excludeUsernames[engagement.username])
 		default:
 			return state
 	}
