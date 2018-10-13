f7808dceda699e0e46d74c77533c818e1b4a3676
Chris Gilson <c.gilson11@gmail.com>
2018-10-13 09:25:05 -0700

    remove givesinvites

--- src/store/actions/invites.js
+++ src/store/actions/invites.js
@@ -20,7 +20,6 @@ export const getInvites = () => async (dispatch) => {
             }
             hideFromUser
             groupID
-            givesInvites
             remainingUses
           }
         }
